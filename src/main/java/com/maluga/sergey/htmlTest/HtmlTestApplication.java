3fc6ed58dbe00d33e3218b6030fd6579282b52a3
Sergey Malyuga <sergey@malyuga.ru>
2024-05-23 16:10:43 +0300

    Init commit

---
+++ src/main/java/com/maluga/sergey/htmlTest/HtmlTestApplication.java
@@ -0,0 +1,13 @@
+package com.maluga.sergey.htmlTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maluga/sergey/htmlTest/HtmlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maluga.sergey.htmlTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
